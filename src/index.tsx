024470bf1e3c15b4f225f5c829b07cbffd6c7291
Natalia Burduk <nataliaburduk@gmail.com>
2023-12-22 11:51:44 +0100

    Connect baseTheme and redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 import App from './App';
+import { ThemeProvider } from 'styled-components';
+import { baseTheme } from './assets/styles/baseTheme';
+import './index.scss';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const rootElement = document.getElementById('root');
+
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={baseTheme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>
 );
